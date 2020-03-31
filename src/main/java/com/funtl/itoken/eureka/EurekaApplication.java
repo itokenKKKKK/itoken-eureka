1888067a349274b3ba41c00daf486f980285d851
skl <1026809406@qq.com>
2020-03-31 16:35:58 +0800

    初始化

---
+++ src/main/java/com/funtl/itoken/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.funtl.itoken.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author GuardianYruler
+ * @create 2020/3/31 16:20
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
